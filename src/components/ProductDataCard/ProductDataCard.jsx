e3cbfe21e7cb83ba8d94b563b61f1f488050a0c5
Hamidullo <hamidullohikma@gmail.com>
2025-07-11 17:23:58 +0500

    pull farrux respons drumm

--- src/components/ProductDataCard/ProductDataCard.jsx
+++ src/components/ProductDataCard/ProductDataCard.jsx
@@ -1,36 +1,50 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./ProductDataCard.scss";
+import { useTranslation } from "react-i18next";
 
 export const ProductDataCard = ({ product }) => {
   const navigate = useNavigate();
+  const { i18n } = useTranslation();
+  const lang = i18n.language;
 
   const handleClick = (productName) => {
     navigate(`/product/details/${encodeURIComponent(productName)}`);
   };
+
+  // Получаем перевод характеристик только из ru и uz
+  const spec =
+    product?.specifications?.translations?.[lang] ||
+    product?.specifications?.translations?.ru || {};
+
+  const name =
+    product?.translations?.[lang]?.name ||
+    product?.translations?.ru?.name ||
+    "Нет названия";
 
   return (
     <div className="product-list">
       <div
         className="product-card"
         id={`product-${product.id}`}
-        onClick={() => handleClick(product.translations.en.name)}
+        onClick={() => handleClick(name)}
       >
         <img
           src={`https://ronixtools.duckdns.org${product.image}`}
-          alt={product.translations?.en?.name}
+          alt={name}
           className="product-image"
         />
 
         <p className="product-code">{product.model}</p>
 
         <div className="product-details">
-          <h3 className="product-title">{product.translations?.en?.name}</h3>
+          <h3 className="product-title">{name}</h3>
+
           <div className="product-specs-row">
-            <span className="spec-item">Battery Chemistry: Lithium</span>
+            <span className="spec-item">Тип: {spec.type || "—"}</span>
           </div>
           <div className="product-specs-row">
-            <span className="spec-item">Battery Voltage: 3.6V</span>
+            <span className="spec-item">Размер: {spec.size || "—"}</span>
           </div>
         </div>
       </div>
